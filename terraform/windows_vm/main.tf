716c653ffd6767952018383d8f1e0dfaeeb62a6f
Cuan Leo <cuanleo@gmail.com>
2023-06-18 15:56:12 +0100

    move terraform files

---
+++ terraform/windows_vm/main.tf
@@ -0,0 +1,42 @@
+resource "hyperv_vhd" "windows_vm_vhd" {
+  path        = var.vhd_path
+  source_disk = var.vhd_source
+  vhd_type    = "Dynamic"
+}
+
+
+resource "hyperv_machine_instance" "windows_vm" {
+  name            = var.vm_name
+  generation      = "2"
+  processor_count = var.vm_processor_count
+  static_memory   = "true"
+  state           = "Running"
+
+  vm_firmware {
+    enable_secure_boot   = "On"
+    secure_boot_template = "MicrosoftWindows"
+    boot_order {
+      boot_type           = "HardDiskDrive"
+      controller_number   = "0"
+      controller_location = "0"
+    }
+    boot_order {
+      boot_type            = "NetworkAdapter"
+      network_adapter_name = "something"
+    }
+  }
+
+  integration_services = {
+    "Guest Service Interface" = true
+    "Heartbeat"               = true
+    "Key-Value Pair Exchange" = true
+    "Shutdown"                = true
+    "Time Synchronization"    = true
+    "VSS"                     = true
+  }
+
+  network_adaptors {
+    name        = var.vm_nic_name
+    switch_name = var.vm_switch_name
+  }
+}
---
+++ terraform/windows_vm/variables.tf
@@ -0,0 +1,53 @@
+variable "provider_user" {
+  type        = string
+  description = "Hyper-V User Account"
+}
+
+variable "provider_password" {
+  type        = string
+  description = "Hyper-V User Password"
+}
+
+variable "provider_host" {
+  type        = string
+  description = "IP/Hostname of Hyper-V Host"
+}
+
+variable "provider_port" {
+  type        = string
+  description = "Port for Hyper-V connection"
+}
+
+variable "vhd_path" {
+  type        = string
+  description = "Path for the destination VHD"
+}
+
+variable "vhd_source" {
+  type        = string
+  description = "Source VHD file"
+}
+
+variable "vm_nic_name" {
+  type        = string
+  description = "Name of the VM NIC"
+}
+
+variable "vm_switch_name" {
+  type        = string
+  description = "Name of the Virtual Switch to use for the VM"
+}
+
+variable "vm_name" {
+  type        = string
+  description = "Name of the VM"
+}
+
+variable "vm_processor_count" {
+  type        = string
+  description = "Number of processors for the VM"
+}
+
+
+
+
